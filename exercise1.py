648c2f1d0510f4a76ce79b8a2e0244bc762a9792
s911128 <jjoce8513@gmail.com>
2025-10-15 23:53:34 +0800

    改良3個初階排序，和新增未完成的quick sort

--- exercise1.py
+++ exercise1.py
@@ -2,13 +2,15 @@ import math
 import random
 import sorting
 
-sequency = [8, 5, 4, 3, 1]
-sequency_bubble_sort = sorting.Bubble_sort(sequency)
-sequency_selection_sort = sorting.Selection_sort(sequency)
-sequency_insertion_sort = sorting.Insetion_sort(sequency)
+sequency = [27, 22 ,20, 25]
+sequency_bubble_sort = sorting.Bubble_sort(list.copy(sequency))
+sequency_selection_sort = sorting.Selection_sort(list.copy(sequency))
+sequency_insertion_sort = sorting.Insetion_sort(list.copy(sequency))
+sequency_quick_sort = sorting.Quick_sort(list.copy(sequency))
 print(sequency)
 print(sequency_bubble_sort)
 print(sequency_selection_sort)
 print(sequency_insertion_sort)
+print(sequency_quick_sort)
 
     
--- sorting.py
+++ sorting.py
@@ -1,51 +1,74 @@
-def Bubble_sort(original_array):
-    array = list.copy(original_array)
+def Bubble_sort(array):
+    temp = 0
     for i in range(1,len(array)):
-        temp = 0
-        isSorted = False
+        isEnd = True
         for j in range(len(array)-i):
             if array[j] > array[j+1]:
                 temp = array[j+1]
                 array[j+1] = array[j]
                 array[j] = temp
-                isSorted = True
-        if not isSorted:
+                isEnd = False
+        if isEnd:
             break
         
     return array
 
-def Selection_sort(original_array):
-    array = list.copy(original_array)
+def Selection_sort(array):
+    temp = 0
     for i in range(len(array)-1):
-        minimum = array[i]
-        index = i
-        isSorted = False
+        smallest = i
         for j in range(i+1,len(array)):
-            if array[j] < minimum:
-                minimum = array[j]
-                index = j
-                isSorted = True
-        if isSorted:
-            array[index] = array[i]
-            array[i] = minimum
+            if array[j] < array[smallest]:
+                smallest = j
         
+        if i != smallest:
+            temp_smallest = array[smallest]
+            array[smallest] = array[i]
+            array[i] = temp_smallest
+
     return array
 
-def Insetion_sort(original_array):
-    array = list.copy(original_array)
+def Insetion_sort(array):
     for i in range(1,len(array)):
         temp = array[i]
         j = i
-        isStop = False
-        while((not isStop) and j!=0):
-            if temp < array[j-1]:
-                array[j] = array[j-1]
-                j = j-1
-            else:
-                isStop = True
+        while(temp < array[j-1] and j > 0):
+            array[j] = array[j-1]
+            j = j-1
         array[j] = temp
 
     return array
-        
+
+def Quick_sort(array):
+    if len(array) > 1:
+        i = 0
+        j = len(array)
+        k = array[0]
+        while(i<j):
+            while(True):
+                i = i+1
+                if i == len(array) or array[i]>k:
+                    break
+            while(True):
+                j = j-1
+                if j == 0 or array[j]<k:
+                    break
+            if i<j:
+                temp = array[i]
+                array[i] = array[j]
+                array[j] = temp
+        array[0] = array[j]
+        array[j] = k
+        if j == 0:
+            return array
+        else:
+            Quick_sort(array[:j])
+        if j == len(array)-1:
+            return array
+        else:
+            Quick_sort(array[j+1:])
+
+    else:
+        return array
 
 
